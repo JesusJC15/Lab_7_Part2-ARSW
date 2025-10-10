19a861eced4a5eb4f0329613edb3e5fdaadb035d
Jesus <jesus.jauregui-c@mail.escuelaing.edu.co>
2025-10-10 18:16:35 -0500

    first commit

---
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016 Pivotal Software, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.arsw.collabpaint;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.lang.NonNull;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class CollabPaintWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+        registry.addEndpoint("/stompendpoint").withSockJS();
+    }
+
+}
---
+++ src/test/java/edu/eci/arsw/msgbroker/WebsocketsMsbrokerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.msgbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebsocketsMsbrokerExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
